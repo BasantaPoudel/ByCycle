bde54f0870c5290caad81da36141088f206010fb
xavier <x@xavier.dev>
2024-07-04 22:26:08 +0200

    Added InsightsCache, a class for storing which insights were shown to the user. Name of the class will probably change because other variables related to the algorithm will probably be stored there too

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -115,3 +115,50 @@ class DailyDataInput {
     );
   }
 }
+
+class InsightsCache {
+  List<Map<String, dynamic>> blood;
+  List<Map<String, dynamic>> backpain;
+  List<Map<String, dynamic>> menstruation;
+  List<Map<String, dynamic>> luteal;
+  List<Map<String, dynamic>> temperature;
+
+  InsightsCache({
+    List<Map<String, dynamic>>? blood,
+    List<Map<String, dynamic>>? backpain,
+    List<Map<String, dynamic>>? menstruation,
+    List<Map<String, dynamic>>? luteal,
+    List<Map<String, dynamic>>? temperature,
+  })  : blood = blood ?? [],
+        backpain = backpain ?? [],
+        menstruation = menstruation ?? [],
+        luteal = luteal ?? [],
+        temperature = temperature ?? [];
+
+  // Convert InsightsCache instance to a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'blood': blood,
+      'backpain': backpain,
+      'menstruation': menstruation,
+      'luteal': luteal,
+      'temperature': temperature,
+    };
+  }
+
+  // Create an InsightsCache instance from a Map
+  factory InsightsCache.fromMap(Map<String, dynamic> map) {
+    return InsightsCache(
+      blood: List<Map<String, dynamic>>.from(map['blood'] ?? []),
+      backpain: List<Map<String, dynamic>>.from(map['backpain'] ?? []),
+      menstruation: List<Map<String, dynamic>>.from(map['menstruation'] ?? []),
+      luteal: List<Map<String, dynamic>>.from(map['luteal'] ?? []),
+      temperature: List<Map<String, dynamic>>.from(map['temperature'] ?? []),
+    );
+  }
+
+  @override
+  String toString() {
+    return toMap().toString();
+  }
+}
