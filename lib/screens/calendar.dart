34f9172d012687f12e1913fa83885e131946b444
xavier <x@xavier.dev>
2024-07-09 11:56:08 +0200

    Added calendar with range selection

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:by_cycle/models/calendar_utils.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -7,19 +10,74 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOn; // Can be toggled on/off by longpressing a date
   DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Calendar'),
+      appBar: AppBar(
+        title: Text('TableCalendar - Range'),
+      ),
+      body: TableCalendar(
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        focusedDay: _focusedDay,
+        selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+        rangeStartDay: _rangeStart,
+        rangeEndDay: _rangeEnd,
+        calendarFormat: _calendarFormat,
+        rangeSelectionMode: _rangeSelectionMode,
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+              _rangeStart = null; // Important to clean those
+              _rangeEnd = null;
+              _rangeSelectionMode = RangeSelectionMode.toggledOff;
+            });
+          }
+        },
+        onRangeSelected: (start, end, focusedDay) {
+          setState(() {
+            _selectedDay = null;
+            _focusedDay = focusedDay;
+            _rangeStart = start;
+            _rangeEnd = end;
+            _rangeSelectionMode = RangeSelectionMode.toggledOn;
+          });
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+        calendarBuilders: CalendarBuilders(
+          dowBuilder: (context, day) {
+            if (day.weekday == DateTime.sunday) {
+              final text = DateFormat.E().format(day);
+
+              return Center(
+                child: Text(
+                  text,
+                  style: TextStyle(color: Colors.red),
+                ),
+              );
+            }
+          },
         ),
-        body: TableCalendar(
-          locale: "en_US",
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-        ));
+      ),
+    );
   }
 }
