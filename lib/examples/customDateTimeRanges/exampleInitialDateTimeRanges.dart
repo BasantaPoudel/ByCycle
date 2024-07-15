4ab03929724abdf65d8a5c8c013f224c4b9e8284
xavier <x@xavier.dev>
2024-07-15 13:39:17 +0200

    Added an example of CustomDateTimeRange to be used in the main.dart for demonstrating how the calendar.dart works

---
+++ lib/examples/customDateTimeRanges/exampleInitialDateTimeRanges.dart
@@ -0,0 +1,29 @@
+import 'package:by_cycle/models/customDateTimeRange.dart';
+
+final List<CustomDateTimeRange> exampleInitialDateTimeRanges = [
+  CustomDateTimeRange(
+    start: DateTime(2024, 07, 16, 13, 05, 46, 0, 12),
+    end: DateTime(2024, 07, 18, 13, 05, 46, 1, 589),
+    phase: 'luteal',
+  ),
+  CustomDateTimeRange(
+    start: DateTime(2024, 07, 19, 13, 05, 46, 1, 593),
+    end: DateTime(2024, 07, 22, 13, 05, 46, 1, 605),
+    phase: 'menstrual',
+  ),
+  CustomDateTimeRange(
+    start: DateTime(2024, 07, 23, 13, 05, 46, 1, 608),
+    end: DateTime(2024, 08, 01, 13, 05, 46, 1, 641),
+    phase: 'follicular',
+  ),
+  CustomDateTimeRange(
+    start: DateTime(2024, 08, 02, 13, 05, 46, 1, 644),
+    end: DateTime(2024, 08, 05, 13, 05, 46, 1, 655),
+    phase: 'ovulatory',
+  ),
+  CustomDateTimeRange(
+    start: DateTime(2024, 08, 06, 13, 05, 46, 1, 658),
+    end: DateTime(2024, 08, 15, 13, 05, 46, 1, 688),
+    phase: 'luteal',
+  ),
+];
