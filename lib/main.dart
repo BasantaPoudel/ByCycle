357f24d8b723afda4f2d2dbc1386e5f2494bd68d
xavier <x@xavier.dev>
2024-07-15 13:48:36 +0200

    Imported CustomDateTimeRange example for Calendar widget so that phases in the calendar are colored by default. Also imported Calendar screen and created a button for entering the Calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:android_intent_plus/android_intent.dart';
 import 'package:android_intent_plus/flag.dart';
 import 'package:by_cycle/cubits/theme/theme_cubit.dart';
+import 'package:by_cycle/examples/customDateTimeRanges/exampleInitialDateTimeRanges.dart';
 import 'package:by_cycle/firebase_options.dart';
+import 'package:by_cycle/screens/calendar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -231,6 +233,43 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             onPressed: openAlarmApp,
             child: const Text('Set Alarm'),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Color(0xFFDED4C5)),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.only(left: 25, right: 25)),
+              textStyle: MaterialStateProperty.all<TextStyle>(
+                  TextStyle(fontSize: 20, color: Colors.black)),
+              elevation: MaterialStateProperty.all<double>(5.0),
+            ),
+            onPressed: () {
+              print(exampleInitialDateTimeRanges);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Calendar(
+                    //initialDateTimeRanges: timeRanges
+                    initialDateTimeRanges: exampleInitialDateTimeRanges,
+                    /*
+                      initialDateTimeRanges: [
+                        DateTimeRange(
+                          start: DateTime.now().add(Duration(days: 7)),
+                          end: DateTime.now().add(Duration(days: 14)),
+                        ),
+                        DateTimeRange(
+                          start: DateTime.now().add(Duration(days: 3)),
+                          end: DateTime.now().add(Duration(days: 5)),
+                        ),
+                      ],
+                      */
+                  ),
+                ),
+              );
+            },
+            child: const Text('Calendar'),
           ),
         ])),
         bottomNavigationBar: BottomNavigationBar(
