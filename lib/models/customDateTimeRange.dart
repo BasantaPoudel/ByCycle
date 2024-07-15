840b2318876f8220aca4a1d6103d3034fb14b34f
xavier <x@xavier.dev>
2024-07-15 13:38:13 +0200

    Added a new class CustomDateTimeRange based on DateTimeRange that additionally contains a property phase to mark which phase a given CustomDateTimeRange describes

---
+++ lib/models/customDateTimeRange.dart
@@ -0,0 +1,39 @@
+class CustomDateTimeRange {
+  /// Creates a date range for the given start and end [DateTime].
+  CustomDateTimeRange({
+    required this.start,
+    required this.end,
+    this.phase = '', // Default phase is an empty string
+  }) : assert(!start.isAfter(end));
+
+  /// The start of the range of dates.
+  final DateTime start;
+
+  /// The end of the range of dates.
+  final DateTime end;
+
+  /// The phase of the date range, one of "menstrual", "follicular", "ovulatory", "luteal", or "" by default.
+  final String phase;
+
+  /// Returns a [Duration] of the time between [start] and [end].
+  ///
+  /// See [DateTime.difference] for more details.
+  Duration get duration => end.difference(start);
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is CustomDateTimeRange &&
+        other.start == start &&
+        other.end == end &&
+        other.phase == phase;
+  }
+
+  @override
+  int get hashCode => Object.hash(start, end, phase);
+
+  @override
+  String toString() => '$start - $end ($phase)';
+}
