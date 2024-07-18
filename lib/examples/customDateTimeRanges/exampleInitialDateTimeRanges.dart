1b597df2dce230c68a2faaf68ee0eab0f70d112f
jakubgrad <113715885+jakubgrad@users.noreply.github.com>
2024-07-18 16:56:53 +0200

    Simplify dates of exampleInitialDateTimeRanges
    Got rid of hours, minutes, seconds etc.

--- lib/examples/customDateTimeRanges/exampleInitialDateTimeRanges.dart
+++ lib/examples/customDateTimeRanges/exampleInitialDateTimeRanges.dart
@@ -2,28 +2,28 @@ import 'package:by_cycle/models/customDateTimeRange.dart';
 
 final List<CustomDateTimeRange> exampleInitialDateTimeRanges = [
   CustomDateTimeRange(
-    start: DateTime(2024, 07, 16, 13, 05, 46, 0, 12),
-    end: DateTime(2024, 07, 18, 13, 05, 46, 1, 589),
+    start: DateTime(2024, 07, 16),
+    end: DateTime(2024, 07, 18),
     phase: 'luteal',
   ),
   CustomDateTimeRange(
-    start: DateTime(2024, 07, 19, 13, 05, 46, 1, 593),
-    end: DateTime(2024, 07, 22, 13, 05, 46, 1, 605),
+    start: DateTime(2024, 07, 19),
+    end: DateTime(2024, 07, 22),
     phase: 'menstrual',
   ),
   CustomDateTimeRange(
-    start: DateTime(2024, 07, 23, 13, 05, 46, 1, 608),
-    end: DateTime(2024, 08, 01, 13, 05, 46, 1, 641),
+    start: DateTime(2024, 07, 23),
+    end: DateTime(2024, 08, 01),
     phase: 'follicular',
   ),
   CustomDateTimeRange(
-    start: DateTime(2024, 08, 02, 13, 05, 46, 1, 644),
-    end: DateTime(2024, 08, 05, 13, 05, 46, 1, 655),
+    start: DateTime(2024, 08, 02),
+    end: DateTime(2024, 08, 05),
     phase: 'ovulatory',
   ),
   CustomDateTimeRange(
-    start: DateTime(2024, 08, 06, 13, 05, 46, 1, 658),
-    end: DateTime(2024, 08, 15, 13, 05, 46, 1, 688),
+    start: DateTime(2024, 08, 06),
+    end: DateTime(2024, 08, 15),
     phase: 'luteal',
   ),
 ];
