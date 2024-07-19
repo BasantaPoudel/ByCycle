482d78906d885c41a749fc432f98da45f1e4143d
xavier <x@xavier.dev>
2024-07-19 20:59:28 +0200

    Created an example of how to run the daily data algorithm

---
+++ lib/examples/algorithm.dart
@@ -0,0 +1,16 @@
+import 'package:by_cycle/algorithms/choose_insight_ids_based_on_list_of_tags.dart';
+import 'package:by_cycle/algorithms/produceTags.dart';
+import 'package:by_cycle/algorithms/redraw_calendar.dart';
+import 'package:by_cycle/algorithms/retrieve_insights_by_their_ids.dart';
+import 'package:by_cycle/algorithms/update_users_algorithm_data.dart';
+import 'package:by_cycle/examples/users/user_after_onboardCalendar.dart';
+
+void main() {
+  var user = user_after_onboardCalendar;
+  updateUsersAlgorithmData(user);
+  List<String> tags = produceTagsForToday(user);
+  redrawCalendar(user, tags);
+  List<String> insightIds = chooseInsightIdsBasedOnListOfTags(user, tags);
+  List<Map<String, dynamic>> insights = retrieveInsightsByTheirIds(insightIds);
+  print(insights);
+}
