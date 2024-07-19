293eb46f8a73f173533ae943accf9093a06c1933
xavier <x@xavier.dev>
2024-07-19 21:23:00 +0200

    Created a function that should be run daily before producing tags so that algorithmData property of the User is up to date

---
+++ lib/algorithms/update_users_algorithm_data.dart
@@ -0,0 +1,50 @@
+import 'package:by_cycle/models/user.dart';
+
+void updateUsersAlgorithmData(User user) {
+/*Returns a user with an updated AlgorithmData property. This function should be run before algorithms
+A-F returning the tags are run to make sure they execute correctly.
+As of now, the function just ensure that algorithmData property has the key-value pair lastFiveDays:
+algorithmData["lastFiveDays"] = 5 dailyDataInputs such that:
+algorithmData["lastFiveDays"][0] <- Today
+algorithmData["lastFiveDays"][1] <- Yesterday
+algorithmData["lastFiveDays"][2] <- 2 days ago etc.
+
+Parameters:
+  -an instance of User
+
+Returns:
+  -Nothing
+
+Side-effects:
+  -user's algorithmData["lastFiveDays"] contains last five days
+*/
+  DateTime today = DateTime.now();
+
+  user.algorithmData["indexOfToday"] =
+      findDailyDataInputIndexByDate(user.dailyDataInput, today);
+
+  print(user.algorithmData["indexOfToday"]);
+
+  user.algorithmData["lastFiveDays"] = [];
+
+  for (int i = 0; i < 5; i++) {
+    user.algorithmData["lastFiveDays"]
+        .add(user.dailyDataInput[user.algorithmData["indexOfToday"] - i]);
+  }
+
+  print(user.algorithmData["lastFiveDays"]);
+}
+
+// a handy function for identifying the index of a dailyDataInput on a specific day.
+int findDailyDataInputIndexByDate(
+    List<DailyDataInput> dailyDataInputs, DateTime targetDate) {
+  for (int i = 0; i < dailyDataInputs.length; i++) {
+    DailyDataInput input = dailyDataInputs[i];
+    if (input.date.year == targetDate.year &&
+        input.date.month == targetDate.month &&
+        input.date.day == targetDate.day) {
+      return i;
+    }
+  }
+  return -1; // Return -1 if no matching date is found
+}
