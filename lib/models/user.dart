c24f5fc0bb02d8a7c32266230ef1b6209620ac7b
xavier <x@xavier.dev>
2024-07-19 21:16:49 +0200

    Added phaseRanges, algorithmData, and tags, a property for guiding the selection of tags and keeping track of user's views of different insights

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,14 @@
+import 'package:by_cycle/models/InsightInfo.dart';
+import 'package:by_cycle/models/custom_date_time_range.dart';
+
 class User {
   String name;
   String email;
   DateTime lastPeriod;
   int menstruationPhaseLength;
+  int follicularPhaseLength;
+  int ovulationPhaseLength;
+  int lutealPhaseLength;
   int completeCycleLength;
   int timeToFallAsleep;
   bool cycleRegular;
@@ -10,12 +16,18 @@ class User {
   bool would_like_reminders_about_data_log_in;
   bool would_like_reminders_about_self_care_checklist;
   List<DailyDataInput> dailyDataInput;
+  List<CustomDateTimeRange> phaseRanges;
+  Map<String, dynamic> algorithmData;
+  Map<String, List<InsightInfo>> tags;
 
   User({
     required this.name,
     required this.email,
     required this.lastPeriod,
     this.menstruationPhaseLength = 0,
+    this.follicularPhaseLength = 0,
+    this.ovulationPhaseLength = 0,
+    this.lutealPhaseLength = 0,
     this.completeCycleLength = 0,
     this.timeToFallAsleep = 20,
     this.cycleRegular = false,
@@ -23,6 +35,9 @@ class User {
     this.would_like_reminders_about_data_log_in = false,
     this.would_like_reminders_about_self_care_checklist = false,
     this.dailyDataInput = const [],
+    this.phaseRanges = const [],
+    this.algorithmData = const {},
+    this.tags = const {},
   });
 
   Map<String, dynamic> toMap() {
@@ -31,6 +46,9 @@ class User {
       'email': email,
       'last_period': lastPeriod,
       'menstuation_phase_length': menstruationPhaseLength,
+      'follicular_phase_length': follicularPhaseLength,
+      'ovulation_phase_length': ovulationPhaseLength,
+      'luteal_phase_length': lutealPhaseLength,
       'complete_cycle_length': completeCycleLength,
       'time_to_fall_asleep': timeToFallAsleep,
       'cycle_regular': cycleRegular,
@@ -40,6 +58,10 @@ class User {
       'would_like_reminders_about_self_care_checklist':
           would_like_reminders_about_self_care_checklist,
       'daily_data_input': dailyDataInput.map((input) => input.toMap()).toList(),
+      'phase_ranges': phaseRanges.map((input) => input.toMap()).toList(),
+      'algorithm_data': algorithmData,
+      'tags': tags.map((key, value) =>
+          MapEntry(key, value.map((tag) => tag.toMap()).toList())),
     };
   }
 
@@ -49,6 +71,9 @@ class User {
       email: map['email'],
       lastPeriod: map['last_period'].toDate(),
       menstruationPhaseLength: map['menstuation_phase_length'],
+      follicularPhaseLength: map['follicular_phase_length'],
+      ovulationPhaseLength: map['ovulation_phase_length'],
+      lutealPhaseLength: map['luteal_phase_length'],
       completeCycleLength: map['complete_cycle_length'],
       timeToFallAsleep: map['time_to_fall_asleep'],
       cycleRegular: map['cycle_regular'],
@@ -62,6 +87,19 @@ class User {
                 ?.map((input) => DailyDataInput.fromMap(input)) ??
             const [],
       ),
+      phaseRanges: List<CustomDateTimeRange>.from(
+        map['phase_ranges']
+                ?.map((input) => CustomDateTimeRange.fromMap(input)) ??
+            const [],
+      ),
+      algorithmData: Map<String, dynamic>.from(map['algorithm_data'] ?? {}),
+      tags: (map['tags'] as Map<String, dynamic>?)?.map(
+            (key, value) => MapEntry(
+                key,
+                List<InsightInfo>.from(
+                    value.map((item) => InsightInfo.fromMap(item)))),
+          ) ??
+          {},
     );
   }
 }
@@ -74,6 +112,7 @@ class DailyDataInput {
   int hoursOfSleep;
   List<String> symptoms;
   double temperature;
+  String phase;
 
   DailyDataInput({
     this.blood = '',
@@ -83,6 +122,8 @@ class DailyDataInput {
     this.hoursOfSleep = 0,
     this.symptoms = const [],
     this.temperature = 0,
+    this.phase =
+        "", // Should be equal to "menstrual", "follicular", "ovulation" or "luteal"
   });
 
   Map<String, dynamic> toMap() {
@@ -94,6 +135,7 @@ class DailyDataInput {
       'hours_of_sleep': hoursOfSleep,
       'symptoms': symptoms,
       'temperature': temperature,
+      'phase': phase,
     };
   }
 
@@ -108,6 +150,12 @@ class DailyDataInput {
           0, // Ensure this matches the stored data type
       symptoms: List<String>.from(map['symptoms'] ?? []),
       temperature: map['temperature'].toDouble() ?? 0,
+      phase: map['phase'] ?? '',
     );
+  }
+
+  @override
+  String toString() {
+    return 'DailyDataInput(blood: $blood, date: $date, discharge: $discharge, energy_level: $energyLevel, hours_of_sleep: $hoursOfSleep, symptoms: $symptoms, temperature: $temperature, phase: $phase)';
   }
 }
