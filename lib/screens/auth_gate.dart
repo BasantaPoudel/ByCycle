e9809513b70011b067407ac7fcaa9c7a51d54777
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2024-08-02 17:55:15 +0300

    Extract ProfilePage

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -2,6 +2,7 @@ import 'package:by_cycle/main.dart';
 import 'package:by_cycle/models/user_model.dart';
 import 'package:by_cycle/repository/user_repository.dart';
 import 'package:by_cycle/screens/feedback.dart';
+import 'package:by_cycle/screens/new_user_profile.dart';
 import 'package:by_cycle/screens/settings.dart';
 import 'package:by_cycle/screens/user_profile.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
@@ -28,60 +29,7 @@ class AuthGate extends StatelessWidget {
             actions: [
               AuthStateChangeAction<UserCreated>((context, state) async {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => ProfileScreen(
-                    appBar: AppBar(
-                        iconTheme:
-                            Theme.of(context).brightness == Brightness.light
-                                ? const IconThemeData(color: Colors.black)
-                                : const IconThemeData(color: Colors.white),
-                        title: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            // crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text('Your Profile'),
-                              // Text(userRepo.loggedInUser!.email!),
-                            ])),
-                    actions: [
-                      SignedOutAction((context) {
-                        RestartWidget.restartApp(context);
-                        Navigator.of(context).pop();
-                      }),
-                      DisplayNameChangedAction(
-                          (context, oldName, newName) async {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            backgroundColor: Colors.green,
-                            content: Text(
-                                'Display name changed to $newName, Please Sign Out and Sign In again once you verify your email!'),
-                            duration: const Duration(seconds: 5),
-                          ),
-                        );
-
-                        await userRepo.updateNewUserToFirestore(UserModel(
-                            name: newName,
-                            email: userRepo.loggedInUser!.email!,
-                            lastPeriod: DateTime.now()));
-
-                        //TODO - Look for alternatives as well
-                      })
-                    ],
-                    children: [
-                      Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            "Please input your name to complete the account creation process. Also, please verify your email and if needed sign out and sign in again!",
-                            style: GoogleFonts.poppins(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
-                              // color: Colors.white,
-                            ),
-                          )),
-                    ],
-                  ),
+                  builder: (context) => NewUserProfile(),
                 ));
               }),
             ],
@@ -129,58 +77,7 @@ class AuthGate extends StatelessWidget {
         } else if (snapshot.data?.displayName != null) {
           return const MyApp();
         }
-        return ProfileScreen(
-          appBar: AppBar(
-              iconTheme: Theme.of(context).brightness == Brightness.light
-                  ? const IconThemeData(color: Colors.black)
-                  : const IconThemeData(color: Colors.white),
-              title: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  // crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text('Your Profile'),
-                    // Text(userRepo.loggedInUser!.email!),
-                  ])),
-          actions: [
-            SignedOutAction((context) {
-              RestartWidget.restartApp(context);
-              Navigator.of(context).pop();
-            }),
-            DisplayNameChangedAction((context, oldName, newName) async {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Colors.green,
-                  content: Text(
-                      'Display name changed to $newName, Please Sign Out and Sign In again once you verify your email!'),
-                  duration: const Duration(seconds: 5),
-                ),
-              );
-
-              await userRepo.updateNewUserToFirestore(UserModel(
-                  name: newName,
-                  email: userRepo.loggedInUser!.email!,
-                  lastPeriod: DateTime.now()));
-
-              //TODO - Look for alternatives as well
-            })
-          ],
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  "Please input your name to complete the account creation process. Also, please verify your email and if needed sign out and sign in again!",
-                  style: GoogleFonts.poppins(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                    // color: Colors.white,
-                  ),
-                )),
-          ],
-        );
+        return NewUserProfile();
       },
     );
   }
---
+++ lib/screens/new_user_profile.dart
@@ -0,0 +1,80 @@
+import 'package:by_cycle/main.dart';
+import 'package:by_cycle/models/user_model.dart';
+import 'package:by_cycle/repository/user_repository.dart';
+import 'package:by_cycle/screens/feedback.dart';
+import 'package:by_cycle/screens/onboarding/onboarding_pageone.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NewUserProfile extends StatefulWidget {
+  @override
+  _NewUserProfileState createState() => _NewUserProfileState();
+}
+
+class _NewUserProfileState extends State<NewUserProfile> {
+  UserRepository userRepo = UserRepository();
+  // final bool _showMessageToNewUser = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProfileScreen(
+      appBar: AppBar(
+          iconTheme: Theme.of(context).brightness == Brightness.light
+              ? const IconThemeData(color: Colors.black)
+              : const IconThemeData(color: Colors.white),
+          title: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text('Your NewUserProfile'),
+                // Text(userRepo.loggedInUser!.email!),
+              ])),
+      actions: [
+        SignedOutAction((context) {
+          RestartWidget.restartApp(context);
+          Navigator.of(context).pop();
+        }),
+        DisplayNameChangedAction((context, oldName, newName) async {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Colors.green,
+              content: Text(
+                  'Display name changed to $newName, Please Sign Out and Sign In again once you verify your email!'),
+              duration: const Duration(seconds: 5),
+            ),
+          );
+
+          await userRepo.updateNewUserToFirestore(UserModel(
+              name: newName,
+              email: userRepo.loggedInUser!.email!,
+              lastPeriod: DateTime.now()));
+
+          //TODO - Look for alternatives as well
+        })
+      ],
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              "Please input your name to complete the account creation process. Also, please verify your email and if needed sign out and sign in again!",
+              style: GoogleFonts.poppins(
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+                // color: Colors.white,
+              ),
+            )),
+      ],
+    );
+  }
+}
